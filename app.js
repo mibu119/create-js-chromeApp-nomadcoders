9372a1ea5e8279ed0799f733ccbba190e27a2bfa
mibu119 <horang296@gmail.com>
2023-05-31 20:18:37 +0900

    4.6 username 불러오기

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const greeting = document.querySelector("#greeting");
 
 // string만 담은 변수는 대문자 변수에 담는다.
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 // 4.2와 4.3 events에 관한 강의 다시 복습하기
 function onLoginSubmit(event) {
@@ -12,8 +13,30 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  // 사용자 이름 localStorage에 저장하기
+  localStorage.setItem(USERNAME_KEY, username);
+  // h1 태그 나타내기
+  // 중복되는 동작은 하나의 함수로 합치자.
+  // greeting.innerText = `Hello ${username}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  // *위의 함수에서 중복됨
+  // greeting.innerText = `Hello ${savedUsername}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(savedUsername);
+}
