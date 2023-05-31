efbc7cd596c0213a23fb85aa2630c444f21413c0
mibu119 <horang296@gmail.com>
2023-05-31 22:36:30 +0900

    📝 5.1 timeout and dates

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,15 @@ const clock = document.querySelector("h2#clock");
 
 // interval 개념
 // 매번 일어나야 하는 것
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getMilliseconds()}`;
 }
 
+// 즉시 getClock을 호출해야 디폴트 값인 00:00:00이 보이지 않음
+getClock();
 // 두 개의 argument를 받음. 첫 번째는 콜백함수, 두 번째는 ms(몇 초마다 호출할 것인지 숫자로 입력)
-setInterval(sayHello, 5000);
+setInterval(getClock, 1000);
+
+// 몇 초후에 나타나게 하고 싶다면 setTimeout을 사용
+// setTimeout();
